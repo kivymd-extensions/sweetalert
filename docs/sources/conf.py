be092aab26152e20937a6ab498a5f5629a80d63c
macbookair <kivydevelopment@gmail.com>
2020-10-27 20:32:22 +0300

    Edit `autoapi_dirs` variable

--- docs/sources/conf.py
+++ docs/sources/conf.py
@@ -49,8 +49,8 @@ extensions = [
 # AutoAPI configuration
 #autoapi_dirs = ["../../../sweetalert"] if os.path.exists(realpath("../../../sweetalert")) else ["../../../../../sweetalert"]
 
-# autoapi_dirs = ["../sweetalert"]  # Not work
-autoapi_dirs = ["sweetalert"]  # Not work
+autoapi_dirs = ["../sweetalert"]  # Not work
+#autoapi_dirs = ["sweetalert"]  # `/home/docs/checkouts/readthedocs.org/user_builds/sweetalert/checkouts/latest/docs/sources/sweetalert` not found
 autoapi_template_dir = os.path.abspath("_templates")
 autoapi_ignore = []
 autoapi_type = "python"
