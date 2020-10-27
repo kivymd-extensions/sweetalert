6d2a5778734e8d184e5e628ff1eae7ca4f105bc0
macbookair <kivydevelopment@gmail.com>
2020-10-27 07:48:50 +0300

    Updated `extras_require`

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         extras_require={
-            "docs": ["sphinx", "kivy", "sphinx-autoapi", "sphinx_rtd_theme"],
+            "docs": ["sphinx", "kivy",  "kivymd", "sphinx-autoapi", "sphinx_rtd_theme"],
         },
         setup_requires=[],
         python_requires=">=3.6",
