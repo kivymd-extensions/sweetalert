94f8106ee9d5c7915c157e2cedd5ec263302888b
macbookair <kivydevelopment@gmail.com>
2020-10-27 07:45:01 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup(
+        extras_require={
+            "docs": ["sphinx", "sphinx-autoapi", "sphinx_rtd_theme"],
+        },
+        setup_requires=[],
+        python_requires=">=3.6",
+    )
